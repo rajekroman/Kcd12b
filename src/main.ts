969f11c238c7986372ab58a62b0fd47989ade852
rajekroman <108610159+rajekroman@users.noreply.github.com>
2026-07-18 14:41:48 +0200

    style: load reputation UI styles

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { registerSW } from 'virtual:pwa-register';
 import { gameConfig } from './game/config';
 import { InventoryUiController } from './game/InventoryUiController';
 import './styles/main.css';
+import './styles/reputation.css';
 
 registerSW({ immediate: true });
 
